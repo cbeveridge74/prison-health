c65c13fa919eb4fa5438a8caecdefd72106a8954
cbeveridge <cbeveridge74@gmail.com>
2025-11-05 09:57:06 +0000

    Updates reflecting Dose parser async nature

--- src/lib/Redux/Utils/Medication/medicationHelper.ts
+++ src/lib/Redux/Utils/Medication/medicationHelper.ts
@@ -6,15 +6,17 @@ export const populateDailyFrequencyAndDailyQuantity = (
     medication : IMedication
 ) : void => {
     const parser = new Parser();
-    const dose: Dose = parser.parse( medication.Dose );
-    console.log( "Dose: " + dose );
-    if( dose.Weekly || dose.Monthly || dose.Yearly ){
-        medication.DailyFrequency = 1 / dose.Frequency; 
-    }else{
-        medication.DailyFrequency = dose.Frequency;
-    }
-    medication.DailyQuantity = dose.Quantity;
-    checkNumDailyDosesMatchNumDailyAdminTimes( medication );
+    parser.parse( medication.Dose ).then((dose: Dose) => {
+        console.log( "Dose: " + dose );
+        if( dose.Weekly || dose.Monthly || dose.Yearly ){
+            medication.DailyFrequency = 1 / dose.Frequency; 
+        }else{
+            medication.DailyFrequency = dose.Frequency;
+        }
+        medication.DailyQuantity = dose.Quantity;
+        checkNumDailyDosesMatchNumDailyAdminTimes( medication );
+    });
+    
 };
 
 export const checkNumDailyDosesMatchNumDailyAdminTimes = (medication : IMedication) => {
--- src/lib/Redux/doseparser.test.ts
+++ src/lib/Redux/doseparser.test.ts
@@ -5,8 +5,11 @@ import { Dose } from './Utils/DoseParser/Dose';
 describe('PH Reducer', () => {
 	it('returns the initial state when nothing is passed', () => {
 		const parser = new Parser();
-		let dose: Dose = parser.parse( "2 tablets twice a day" );
-		expect(dose.Quantity).toBe(2);
-		expect(dose.Frequency).toBe(2);
+		//let dose: Dose = null as any;
+		parser.parse( "2 tablets thrice a day" ).then( (dose: Dose) => {
+			expect(dose.Quantity).toBe(2);
+			expect(dose.Frequency).toBe(3);
+		} );
+		
 	});
 });
